973d47bedb27fc39ca5b30895e736ab45167fde1
Aleksei Smirnov <tlalok@inbox.ru>
2023-09-07 13:05:10 +0300

    Refactor src folders

--- src/Gimpo.Data.Analysis.DataFrame/NumericColumns/Computation/NumericArithmeticComputation.cs
+++ src/Gimpo.Data.Analysis.DataFrame/NumericColumns/Computation/NumericArithmeticComputation.cs
@@ -10,34 +10,34 @@ namespace Gimpo.Data.Analysis
     public abstract class NumericArithmeticComputation<T>  where T : unmanaged
     {
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<double> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<double> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<double> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<float> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<float> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<float> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<sbyte> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<sbyte> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<sbyte> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<byte> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<byte> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<byte> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<short> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<short> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<short> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<ushort> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<ushort> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<ushort> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<int> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<int> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<int> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<uint> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<uint> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<uint> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<long> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<long> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<long> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
         public virtual void Add(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<ulong> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
-        public virtual void ReverseAdd(NativeMemoryNullableVector<ulong> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => Add(right, left, result);
+        public virtual void ReverseAdd(NativeMemoryNullableVector<ulong> left, NativeMemoryNullableVector<T> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
 
 
         public virtual void Substract(NativeMemoryNullableVector<T> left, NativeMemoryNullableVector<double> right, NativeMemoryNullableVector<T> result) => throw new NotSupportedException();
